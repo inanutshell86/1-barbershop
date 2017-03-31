4074fc56f127cc11d258e7235b4be27aceb015b1
Ihor Khrystiuk <inanutshell86@gmail.com>
2017-03-31 17:40:15 +0300

    replace func for btns show menu in the end of the file

--- src/js/main.js
+++ src/js/main.js
@@ -14,25 +14,6 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-// Button show news
-var btnShowAll = document.querySelector(".news__to-all");
-var news = document.querySelectorAll(".news__item");
-
-btnShowAll.addEventListener('click', function(a) {
-  a.preventDefault();
-  [].forEach.call(news, function(elem) {
-    if (elem.classList.contains('news__item--closed')) {
-      btnShowAll.innerHTML = 'Скрыть все';
-      elem.classList.remove('news__item--closed');
-      elem.classList.add('news__item--opened');
-    } else {
-      btnShowAll.innerHTML = 'Показать все';
-      elem.classList.add('news__item--closed');
-      elem.classList.remove('news__item--opened');
-    }
-  });
-});
-
 // Show modal form window
 var btnLogin = document.querySelector('.main-nav__user-login');
 var modalForm = document.querySelector('.modal-personal');
@@ -51,4 +32,23 @@ var btnClose = document.querySelector('.btn--close');
 btnClose.addEventListener('click', function closeModalForm(a) {
   a.preventDefault();
   modalForm.classList.remove('modal-personal--show');
+});
+
+// Button show news
+var btnShowAll = document.querySelector(".news__to-all");
+var news = document.querySelectorAll(".news__item");
+
+btnShowAll.addEventListener('click', function(a) {
+  a.preventDefault();
+  [].forEach.call(news, function(elem) {
+    if (elem.classList.contains('news__item--closed')) {
+      btnShowAll.innerHTML = 'Скрыть все';
+      elem.classList.remove('news__item--closed');
+      elem.classList.add('news__item--opened');
+    } else {
+      btnShowAll.innerHTML = 'Показать все';
+      elem.classList.add('news__item--closed');
+      elem.classList.remove('news__item--opened');
+    }
+  });
 });
